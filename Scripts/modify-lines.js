6df32581f6c0a35aeecfb9ce4db9edc654d395be
Ian Beck <ian@onecrayon.com>
2017-02-22 13:57:14 -0800

    Removed selection editing actions in favor of Espresso 3 multiple cursors
    * Completely removed multiple selection editing actions, as they are superfluous in Espresso 3
    * Reworked selected line editing actions to use multiple cursors and work across multiple selections
    * Line editing actions now use shortcuts from selection editing actions

--- Scripts/modify-lines.js
+++ Scripts/modify-lines.js
@@ -8,11 +8,14 @@
  * - target (string): where to insert the new characters; 'start', 'end', or 'both'
  */
 
-var escSnippetChars = loadLibrary('handy-lib').escSnippetChars;
-
-action.canPerformWithContext = function(context, outError) {
-	// Only perform the action if we have a selection
-	return context.selectedRanges.length === 1 && context.selectedRanges[0].length > 0;
+action.titleWithContext = function(context, outError) {
+	var fullRange = context.selectedRanges[0];
+	if (context.selectedRanges.length > 1) {
+		var finalRange = context.selectedRanges[context.selectedRanges.length - 1];
+		fullRange = new Range(fullRange.location, finalRange.location + finalRange.length - fullRange.location)
+	}
+	var linebreaks = context.substringWithRange(fullRange).split(/[\r\n]+/).length - 1;
+	return (linebreaks > 1 ? "@multiple" : null);
 };
 
 action.performWithContext = function(context, outError) {
@@ -20,26 +23,30 @@ action.performWithContext = function(context, outError) {
 	var target = action.setup.target.toLowerCase(),
 		prefixLines = (target === 'start' || target === 'both' ? true : false),
 		suffixLines = (target === 'end' || target === 'both' ? true : false),
-		lineRange = context.lineStorage.lineRangeForRange(context.selectedRanges[0]),
-		snippet = escSnippetChars(context.substringWithRange(lineRange));
+		lineRange, lines, line, result, curLocation,
+		cursors = [];
 	
-	// This function does the actual parsing
-	var insertSnippetVariables = function(match, leadingWS, content, trailingWS) {
-		var returnStr = leadingWS;
-		if (prefixLines) {
-			returnStr += '${1}';
+	for (var i = 0, count = context.selectedRanges.length; i < count; i++) {
+		lineRange = context.lineStorage.lineRangeForRange(context.selectedRanges[i]);
+		curLocation = lineRange.location;
+		lines = context.substringWithRange(lineRange).match(/^.*?([\n\r]+|$)/gm);
+		
+		if (!lines) return false;
+		
+		for (var j = 0, lineCount = lines.length; j < lineCount; j++) {
+			line = lines[j];
+			result = /^(\s*)(\S.*?)(\s*)$/.exec(line);
+			if (result) {
+				if (prefixLines) {
+					cursors.push(new Range(curLocation + result[1].length, 0))
+				}
+				if (suffixLines) {
+					cursors.push(new Range(curLocation + result[1].length + result[2].length, 0))
+				}
+			}
+			curLocation += line.length;
 		}
-		returnStr += content;
-		if (suffixLines) {
-			returnStr += '${1}';
-		}
-		returnStr += trailingWS;
-		return returnStr;
-	};
-	// Run our snippet through the parsing function
-	snippet = snippet.replace(/^(\s*)(\S.*?)([ \t]*$[\n\r]*)/gm, insertSnippetVariables);
-	console.log(snippet);
-	// And insert it, making sure not to re-indent things
-	context.selectedRanges = [lineRange];
-	context.insertTextSnippet(new CETextSnippet(snippet), CETextOptionVerbatim);
+	}
+	context.selectedRanges = cursors;
+	return true;
 };
